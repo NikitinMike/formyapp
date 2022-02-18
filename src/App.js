669cee6e6ea93e68b70303052d57788982e980e1
mike <jobles11@gmail.com>
2022-02-18 16:40:23 +0300

    split App to classes Form and Data

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import React from "react";
 class Data extends React.Component {
     render(props) {
         const col=this.props.col
-        const data=this.props.data
+        // const data=this.props.data
+        const row=this.props.row
+        // console.log(col,':',row)
+        const val = row[col]
         return (
             <label htmlFor={col}>{col}&nbsp;
-                <input name={col} id={col} value={data} onChange={this.handleChange} />
+                <input name={col} id={col} value={val} onChange={this.handleChange} />
                 {/*onChange={(e)=>editRow(e,col)}/>*/}
                 <br/>
             </label>
@@ -33,13 +36,13 @@ class Form extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 {/*<label>Имя:</label>*/}
                 {/*<input name={"name"} />*/}
-                <Data col={'id'} data={row.id}/>
-                <Data col={'firstName'} data={row['firstName']}/>
-                <Data col={'lastName'} data={row['lastName']}/>
-                <Data col={'email'} data={row['email']}/>
-                <Data col={'phone'} data={row['phone']}/>
-                <Data col={'city'} data={row['city']}/>
-                <Data col={'country'} data={row['country']}/>
+                <Data col={'id'} data={row['id']} row={row}/>
+                <Data col={'firstName'} data={row['firstName']} row={row}/>
+                <Data col={'lastName'} data={row['lastName']} row={row}/>
+                <Data col={'email'} data={row['email']} row={row}/>
+                <Data col={'phone'} data={row['phone']} row={row}/>
+                <Data col={'city'} data={row['city']} row={row}/>
+                <Data col={'country'} data={row['country']} row={row}/>
                 <br/>
                 <input type="submit" value="Отправить"/>
                 {/*<center><button>Submit</button></center>*/}
@@ -66,7 +69,7 @@ class App extends React.Component {
 
     render() {
         const row = this.state.rows[0]
-        console.log(this.state.rows)
+        // console.log(this.state.rows)
         if(this.state.isLoaded)
             return (<div className="Contacts"><Form row={row}/></div>)
         else return  (<div className="Contacts"> </div>)
