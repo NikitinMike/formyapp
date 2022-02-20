753957bf60cb281ae2ec0c88a2ce4998de888986
mike <jobles11@gmail.com>
2022-02-20 23:19:38 +0300

    move on records

--- src/App.js
+++ src/App.js
@@ -40,11 +40,12 @@ class Form extends React.Component {
     super(props);
     this.state = {row: this.props.row};
     this.handleSubmit = this.handleSubmit.bind(this);
-    // console.log('FORM DATA:',this.state.row);
+    // console.log('FORM DATA:',this.state);
   }
 
   inputColumn(col) {
-    return (<Data col={col} val={this.state.row[col]} row={this.state.row}/>)
+    const row = this.state.row // s[this.state.index]
+    return (<Data col={col} val={row[col]} row={row}/>)
   }
 
   render(props) {
@@ -57,16 +58,19 @@ class Form extends React.Component {
         {this.inputColumn('phone')}
         {this.inputColumn('city')}
         {this.inputColumn('country')}
-        <br/><center><input type="submit" value="Отправить"/></center>
+        <br/>
+        <center>
+          <input type="submit" value="Отправить"/>
+        </center>
       </form>
     )
   }
 
-  async handleSubmit() {
-    const row = this.state.row
+  handleSubmit() {
+    const row = this.state.row // s[this.state.index]
     const id = row.id
     console.log("SUBMIT DATA:", row)
-    await fetch('http://localhost:3000/contacts/' + id, {
+    fetch('http://localhost:3000/contacts/' + id, {
       body: JSON.stringify(row),
       method: 'PUT',
       headers: {
@@ -74,11 +78,50 @@ class Form extends React.Component {
         'Content-Type': 'application/json'
       },
     })
-      .then(response => response.json())
+      // .then(response => response.json())
       .then(data => console.log(data))
       .catch(e => console.log(e))
   }
 
+}
+
+class Page extends React.Component {
+
+  moveNext() {
+    const index = this.state.index + 1
+    const row = this.state.rows[index]
+    this.setState({index: index,row:row})
+    console.log(this.state)
+  }
+
+  moveBack() {
+    const index = this.state.index - 1
+    const row = this.state.rows[index]
+    this.setState({index: index,row:row })
+    console.log(this.state)
+  }
+
+  constructor(props) {
+    super(props);
+    this.moveNext = this.moveNext.bind(this);
+    this.moveBack = this.moveBack.bind(this);
+    this.state = {rows: this.props.rows, index: 0, row:this.props.rows[0]};
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <Form row={this.state.row}/>
+        <br/>
+        <center>
+          <input type="button" value="back" onClick={this.moveBack}/>
+          <label> -------------------- </label>
+          <input type="button" value="forward" onClick={this.moveNext}/>
+        </center>
+      </div>
+    );
+  }
 }
 
 class App extends React.Component {
@@ -90,8 +133,7 @@ class App extends React.Component {
 
   render() {
     // console.log(this.state.rows)
-    const row = this.state.rows[0]
-    const body = this.state.isLoaded ? <Form row={row}/> : null
+    const body = this.state.isLoaded ? <Page rows={this.state.rows}/> : null
     return (<div className="Contacts">{body}</div>)
   }
 
